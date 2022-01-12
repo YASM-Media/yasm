c31f551af9aedc75572b02ab8c4217da707b3736
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-12 08:10:31 +0530

    Controller Implementation for creating new story. Closes #103

--- server/src/modules/story/story.controller.ts
+++ server/src/modules/story/story.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { LoggedInUser } from 'src/decorators/logged-in-user.decorator';
+import { CreateStoryDto } from 'src/DTOs/story/create-story.dto';
+import { FirebaseAuthGuard } from 'src/guards/firebase-auth.guard';
+import { Story } from 'src/models/story.model';
+import { User } from 'src/models/user.model';
+import { StoryService } from './story.service';
 
 @Controller('story')
-export class StoryController {}
+@UseGuards(FirebaseAuthGuard)
+export class StoryController {
+  constructor(private readonly storyService: StoryService) {}
+
+  @Post()
+  public async createStory(
+    @LoggedInUser() user: User,
+    @Body() createStoryDto: CreateStoryDto,
+  ): Promise<Story> {
+    return await this.storyService.createStory(user, createStoryDto);
+  }
+}
