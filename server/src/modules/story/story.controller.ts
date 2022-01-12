7b8d10fe2038b32e149478eef94aac02e9dc22c9
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-12 08:20:00 +0530

    Controller Implementation for deleting story. Closes #106

--- server/src/modules/story/story.controller.ts
+++ server/src/modules/story/story.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { DeleteStoryDto } from 'src/DTOs/story/delete-story.dto';
+import { Body, Controller, Delete, Post, UseGuards } from '@nestjs/common';
 import { LoggedInUser } from 'src/decorators/logged-in-user.decorator';
 import { CreateStoryDto } from 'src/DTOs/story/create-story.dto';
 import { FirebaseAuthGuard } from 'src/guards/firebase-auth.guard';
@@ -17,5 +18,13 @@ export class StoryController {
     @Body() createStoryDto: CreateStoryDto,
   ): Promise<Story> {
     return await this.storyService.createStory(user, createStoryDto);
+  }
+
+  @Delete()
+  public async deleteStory(
+    @LoggedInUser() user: User,
+    @Body() deleteStoryDto: DeleteStoryDto,
+  ): Promise<Story> {
+    return await this.storyService.deleteStory(user, deleteStoryDto);
   }
 }
