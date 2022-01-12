e37fdfa4fe27ac83fab83abdaf307d020147cc0a
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-12 08:10:18 +0530

    Service Implementation for creating new story. Closes #102

--- server/src/modules/story/story.service.ts
+++ server/src/modules/story/story.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateStoryDto } from 'src/DTOs/story/create-story.dto';
+import { Story } from 'src/models/story.model';
+import { User } from 'src/models/user.model';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class StoryService {}
+export class StoryService {
+  constructor(
+    @InjectRepository(Story)
+    private readonly storyRepository: Repository<Story>,
+  ) {}
+
+  public async createStory(
+    user: User,
+    createStoryDto: CreateStoryDto,
+  ): Promise<Story> {
+    const newStory: Story = new Story();
+    newStory.storyUrl = createStoryDto.storyUrl;
+    newStory.user = user;
+
+    return await this.storyRepository.save(newStory);
+  }
+}
