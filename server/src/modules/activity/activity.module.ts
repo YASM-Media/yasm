bba15934c961954d8cd912bcdcbc6153b65573c8
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-02 09:27:44 +0530

    Controller Implementation for fetching activity. Closes #128

--- server/src/modules/activity/activity.module.ts
+++ server/src/modules/activity/activity.module.ts
@@ -3,10 +3,12 @@ import { Activity } from './../../models/activity.model';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ActivityService } from './activity.service';
+import { ActivityController } from './activity.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Activity]), NotificationModule],
   providers: [ActivityService],
   exports: [ActivityService],
+  controllers: [ActivityController],
 })
 export class ActivityModule {}
