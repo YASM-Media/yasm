3d4fe2db82512bb42d0a7cb22895d2122f6e20c1
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-09 08:03:46 +0530

    Model Implementation for thread. Closes #134

---
+++ client/src/models/thread.model.ts
@@ -0,0 +1,41 @@
+import { Chat } from './chat.model';
+import { Timestamp } from 'firebase/firestore';
+
+export class Thread {
+  public id: string = '';
+  public participants: string[] = [];
+  public messages: Chat[] = [];
+  public seen: string[] = [];
+
+  static fromJson(json: any): Thread {
+    const thread = new Thread();
+
+    thread.id = json.id ?? '';
+    thread.participants = json.participants ?? '';
+    thread.messages = json.messages
+      ? json.messages.map((message: any) => Chat.fromJson(message))
+      : [];
+    thread.seen = json.seen ?? [];
+
+    return thread;
+  }
+
+  toJson(): {
+    id: string;
+    participants: string[];
+    messages: {
+      id: string;
+      userId: string;
+      message: string;
+      createdAt: Timestamp;
+    }[];
+    seen: string[];
+  } {
+    return {
+      id: this.id,
+      participants: this.participants,
+      messages: this.messages.map((message) => message.toJson()),
+      seen: this.seen,
+    };
+  }
+}
