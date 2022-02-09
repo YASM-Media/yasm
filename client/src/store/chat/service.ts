e64a13e28fab5070a411f09423ce7c4b3e7f624b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-09 08:34:45 +0530

    Service Implementation for marking chats seen. Closes #137

--- client/src/store/chat/service.ts
+++ client/src/store/chat/service.ts
@@ -5,8 +5,10 @@ import {
   DocumentReference,
   Query,
   query,
+  setDoc,
   where,
 } from 'firebase/firestore';
+import { Thread } from '../../models/thread.model';
 import { firebaseAuth, firebaseFirestore } from './../../utils/firebase';
 
 export const listenToThreads = (): Query<DocumentData> => {
@@ -26,4 +28,22 @@ export const listenToThread = (
   const threadDocument = doc(firebaseFirestore, 'threads', threadId);
 
   return threadDocument;
+};
+
+export const markThreadSeen = async (thread: Thread): Promise<void> => {
+  try {
+    const userId = firebaseAuth.currentUser!.uid;
+
+    if (thread.seen.filter((id) => id === userId).length === 1) {
+      return;
+    }
+
+    thread.seen.push(userId);
+    const rawThread = thread.toJson();
+
+    await setDoc(doc(firebaseFirestore, 'threads', thread.id), rawThread);
+  } catch (error) {
+    console.log(error);
+    throw new Error('Something went wrong, please try again later');
+  }
 };
