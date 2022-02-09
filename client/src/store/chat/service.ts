7e8986ff5c5b2caf92a9a37c44b5d8f7eea2654d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-09 09:04:11 +0530

    Checking for duplicate threads

--- client/src/store/chat/service.ts
+++ client/src/store/chat/service.ts
@@ -3,6 +3,7 @@ import {
   doc,
   DocumentData,
   DocumentReference,
+  getDocs,
   Query,
   query,
   setDoc,
@@ -10,6 +11,7 @@ import {
 } from 'firebase/firestore';
 import { Thread } from '../../models/thread.model';
 import { firebaseAuth, firebaseFirestore } from './../../utils/firebase';
+import { v4 } from 'uuid';
 
 export const listenToThreads = (): Query<DocumentData> => {
   const userId = firebaseAuth.currentUser!.uid;
@@ -45,5 +47,39 @@ export const markThreadSeen = async (thread: Thread): Promise<void> => {
   } catch (error) {
     console.log(error);
     throw new Error('Something went wrong, please try again later');
+  }
+};
+
+const checkForDuplicates = async (participants: string[]): Promise<string> => {
+  const originalParticipants = [...participants];
+  const reversedParticipants = [...participants.reverse()];
+
+  const firstThreadQuerySnapshot = query(
+    collection(firebaseFirestore, 'threads'),
+    where('participants', 'in', [originalParticipants])
+  );
+
+  const firstThreadDocuments = await (
+    await getDocs(firstThreadQuerySnapshot)
+  ).docs;
+
+  const secondThreadQuerySnapshot = query(
+    collection(firebaseFirestore, 'threads'),
+    where('participants', 'in', [reversedParticipants])
+  );
+
+  const secondThreadDocuments = await (
+    await getDocs(secondThreadQuerySnapshot)
+  ).docs;
+
+  if (firstThreadDocuments.length !== 0 && firstThreadDocuments[0].exists) {
+    return firstThreadDocuments[0].id;
+  } else if (
+    secondThreadDocuments.length !== 0 &&
+    secondThreadDocuments[0].exists
+  ) {
+    return secondThreadDocuments[0].id;
+  } else {
+    return 'EMPTY';
   }
 };
