b13b595a6b981047c52b9e7811a5d28e45c69253
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-23 07:20:15 +0530

    Post Model Refactor

--- client/src/models/post.model.ts
+++ client/src/models/post.model.ts
@@ -3,13 +3,90 @@ import { Like } from './like.model';
 import { User } from './user.model';
 
 export class Post {
+  public id: string = '';
+  public text: string = '';
+  public createdAt: Date = new Date(Date.now());
+  public user: User = User.newEmptyUser();
+  public images: Image[] = [];
+  public likes: Like[] = [];
+  public comments: Post[] = [];
+
+  static newPost(): Post {
+    const post = new Post(
+      '',
+      '',
+      new Date(Date.now()),
+      User.newEmptyUser(),
+      [],
+      [],
+      []
+    );
+
+    return post;
+  }
+
   constructor(
-    public id: string,
-    public text: string,
-    public createdAt: Date,
-    public user: User,
-    public images: Image[],
-    public likes: Like[],
-    public comments: Post[]
-  ) {}
+    id: string,
+    text: string,
+    createdAt: Date,
+    user: User,
+    images: Image[],
+    likes: Like[],
+    comments: Post[]
+  ) {
+    this.id = id;
+    this.text = text;
+    this.createdAt = createdAt;
+    this.user = user;
+    this.images = images;
+    this.likes = likes;
+    this.comments = comments;
+  }
+
+  static fromJson(json: any): Post {
+    const post = Post.newPost();
+
+    return post;
+  }
+
+  toJson(): {
+    id: string;
+    text: string;
+    createdAt: Date;
+    user: {
+      id: string;
+      firstName: string;
+      lastName: string;
+      biography: string;
+      imageUrl: string;
+      emailAddress: string;
+      followers: any[];
+      following: any[];
+    };
+    images: { id: string; imageUrl: string }[];
+    likes: {
+      id: string;
+      user: {
+        id: string;
+        firstName: string;
+        lastName: string;
+        biography: string;
+        imageUrl: string;
+        emailAddress: string;
+        followers: any[];
+        following: any[];
+      };
+    }[];
+    comments: any[];
+  } {
+    return {
+      id: this.id,
+      text: this.text,
+      createdAt: this.createdAt,
+      user: this.user.toJson(),
+      images: this.images.map((image) => image.toJson()),
+      likes: this.likes.map((like) => like.toJson()),
+      comments: this.comments.map((comment) => comment.toJson()),
+    };
+  }
 }
